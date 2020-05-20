cc95d3924389e456f5da2a26b17822e04a272c36
kennknisley <58495936+kennknisley@users.noreply.github.com>
2020-05-20 18:36:00 -0500

    final push

--- Instructions/static/js/logic.js
+++ Instructions/static/js/logic.js
@@ -4,19 +4,19 @@ var API_KEY = "pk.eyJ1Ijoia2VubmtuaXNsZXkiLCJhIjoiY2thNjFiejA4MDMzMDJ4b2VwNHZmZ2
 var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
-  id: "mapbox.streets",
+  id: "mapbox.streets-satellite",
   accessToken: API_KEY
 });
 
-var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.dark",
-  accessToken: API_KEY
-});
+// var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "mapbox.dark",
+//   accessToken: API_KEY
+// });
 
 // Create our map, giving it the streetmap and earthquakes layers to display on load
-var myMap = L.map("mapid", {
+var myMap = L.map("map", {
   center: [
     37.09, -95.71
   ],
@@ -59,8 +59,8 @@ function createMap(earthquakes) {
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
-    "Street Map": streetmap,
-    "Dark Map": darkmap
+    "Street Map": streetmap
+    // "Dark Map": darkmap
   };
 
   // Create overlay object to hold our overlay layer
